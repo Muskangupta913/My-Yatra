42addda2a8fa6c7675c2bb16f670fed27fc6bbf9
Muskan Gupta <arohigupta367@gmail.com>
2024-12-02 14:00:03 +0530

    ok

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -22,17 +22,9 @@ public function login(Request $request){
         return back()->withErrors([
             'email'=>'The provided crendentials do not match'])->withInput($request ->only('email', 'remember'))
         }
+lets discusse the difference between mern stack and laravel , php so lets start from here . mern stack is a combination of react , node js , express js , mongodb 
+where as php and laravel is differet language meanwhile the function of both of them is similar only the term and condition make them different is syntax of <code class="so lets disscuss each of lang"></code>
 
-
-        public login(Request $request){
-            ValidateData=$request->validate([
-                'email'='require|email',
-                'password'='require|min6',
-                ])
-
-                if(Auth::attempt(['email'=>$request->email, 'password'=>$request->password])){
-                    return redirect()->intended();
-                }
-                return back()->withErrors([
-                    'email'])
-        }
+react js - it is used for building client side application ehich helps to make a website single page web application
+node js - it is java script run time envirnment l
+    
